41fa8d76c6e012bc7c2956084273b35f31f2ef99
zhudaidai <1010462584@qq.com>
2022-06-29 15:21:44 +0800

    login第二次保存

--- src/App.vue
+++ src/App.vue
@@ -3,11 +3,16 @@
    <h1>{{ msg }}</h1>
     <h2>{{ obj.name }}</h2>
     <h3>{{ obj.age > 18 ? '成年' : '未成年' }}</h3>
+
+    <a v-bind:href="url">点击去百度</a>
+    <!-- 语法: :原生属性名="vue变量" -->
+    <img :src="imgUrl" />
+    <img :src="localImg">
   </div>
 </template>
 
 <script>
-
+import imgObj from './assets/logo.png'
 
 export default {
   data() { // 格式固定, 定义vue数据之处
@@ -15,20 +20,24 @@ export default {
       msg: "hello, vue",
       obj: {
         name: "小vue",
-        age: 5
-      }
+        age: 5,
+          },
+        url: 'http://www.baidu.com',
+        imgUrl: "http://yun.itheima.com/Upload/./Images/20210412/60741c11ab77b.jpg",
+        localImg: imgObj
     }
+      
   }
 }
 </script>
 
 <style>
-#app {
+/* #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
-}
+} */
 </style>
