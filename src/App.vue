ff5cbcc97ee4b768dd2dfabec1c9ed4044186749
zhudaidai <1010462584@qq.com>
2022-06-29 16:48:18 +0800

    vue 翻转

--- src/App.vue
+++ src/App.vue
@@ -1,43 +1,37 @@
 <template>
-  <div id="app">
-   <h1>{{ msg }}</h1>
-    <h2>{{ obj.name }}</h2>
-    <h3>{{ obj.age > 18 ? '成年' : '未成年' }}</h3>
-
-    <a v-bind:href="url">点击去百度</a>
-    <!-- 语法: :原生属性名="vue变量" -->
-    <img :src="imgUrl" />
-    <img :src="localImg">
+  <!-- 思路
+  1. 变量准备-静态页面铺设
+  2. 绑定点击事件 
+  3. 截取字符串返回数组
+  4. 翻转
+  5. 数组拼接起来 -->
+  <div>
+    <!-- 1. 变量准备-静态页面铺设 -->
+    <h1>{{ message }}</h1>
+     <!-- 2. 绑定点击事件 -->
+    <button @click="btn">逆转世界</button>
   </div>
 </template>
 
 <script>
-import imgObj from './assets/logo.png'
-
 export default {
-  data() { // 格式固定, 定义vue数据之处
-    return {  // key相当于变量名
-      msg: "hello, vue",
-      obj: {
-        name: "小vue",
-        age: 5,
-          },
-        url: 'http://www.baidu.com',
-        imgUrl: "http://yun.itheima.com/Upload/./Images/20210412/60741c11ab77b.jpg",
-        localImg: imgObj
-    }
-      
-  }
-}
-</script>
+  data() {
+    return {
+      message: "HELLO, WORLD",
+    };
+  },
+  methods: {
+    btn(){
+            // // 3. 截取字符串返回数组
+      // let arr = this.message.split("")
+      // // 4. 翻转
+      // arr.reverse()
+      // // 5. 数组拼接起来
+      // this.message = arr.join("")
 
-<style>
-/* #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-} */
-</style>
+      // 简化
+      this.message = this.message.split("").reverse().join("")
+    }
+  }
+};
+</script>
