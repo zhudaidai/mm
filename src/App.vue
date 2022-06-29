5aacc3de947e3686e141810cb571c8444a653554
zhudaidai <1010462584@qq.com>
2022-06-29 16:09:18 +0800

    vue  v-on 绑定指令

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,39 @@
 <template>
-  <div id="app">
-   <h1>{{ msg }}</h1>
-    <h2>{{ obj.name }}</h2>
-    <h3>{{ obj.age > 18 ? '成年' : '未成年' }}</h3>
-
-    <a v-bind:href="url">点击去百度</a>
-    <!-- 语法: :原生属性名="vue变量" -->
-    <img :src="imgUrl" />
-    <img :src="localImg">
+  <div>
+    <p>商品总数 {{ count }}</p>
+    <button v-on:click="count++">+1</button>
+    <button v-on:click="addFn">+1</button>
+    <button v-on:click="addFn1(5)">+5</button>
+    <button @click="subFn">-1</button>
   </div>
 </template>
 
 <script>
-import imgObj from './assets/logo.png'
-
 export default {
-  data() { // 格式固定, 定义vue数据之处
-    return {  // key相当于变量名
-      msg: "hello, vue",
-      obj: {
-        name: "小vue",
-        age: 5,
-          },
-        url: 'http://www.baidu.com',
-        imgUrl: "http://yun.itheima.com/Upload/./Images/20210412/60741c11ab77b.jpg",
-        localImg: imgObj
-    }
-      
-  }
-}
+  name: "App",
+  data() {
+    // 格式固定, 定义vue数据之处
+    return {
+      // key相当于变量名
+      count: 0,
+    };
+  },
+  methods: {
+    // vue 的方法写在 里边
+    addFn() {
+      // this代表export default后面的组件对象(下属有data里return出来的属性)
+      this.count++;
+      // this 要注意一下
+    },
+    addFn1(val) {
+      console.log(val);
+      this.count += 5;
+    },
+    subFn() {
+      this.count--;
+    },
+  },
+};
 </script>
 
 <style>
