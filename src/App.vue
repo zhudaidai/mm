c3dcb41f87956f0683464f679767f45226ce731e
zhudaidai <1010462584@qq.com>
2022-06-29 16:21:36 +0800

    v-on 事件对象

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,32 @@
 <template>
   <div id="app">
-   <h1>{{ msg }}</h1>
-    <h2>{{ obj.name }}</h2>
-    <h3>{{ obj.age > 18 ? '成年' : '未成年' }}</h3>
-
-    <a v-bind:href="url">点击去百度</a>
-    <!-- 语法: :原生属性名="vue变量" -->
-    <img :src="imgUrl" />
-    <img :src="localImg">
+     <a @click="one" href="http://www.baidu.com">阻止百度</a>
+    <hr>
+    <a @click="two(10, $event)" href="http://www.baidu.com">阻止去百度</a>
   </div>
 </template>
 
 <script>
-import imgObj from './assets/logo.png'
+
 
 export default {
   data() { // 格式固定, 定义vue数据之处
     return {  // key相当于变量名
-      msg: "hello, vue",
-      obj: {
-        name: "小vue",
-        age: 5,
-          },
-        url: 'http://www.baidu.com',
-        imgUrl: "http://yun.itheima.com/Upload/./Images/20210412/60741c11ab77b.jpg",
-        localImg: imgObj
+     
     }
       
+  },
+  methods: {
+    // 1. 事件触发, 无传值, 可以直接获取事件对象
+    one(e){
+      e.preventDefault()
+    },
+    // 2. 事件触发, 传值, 需要手动传入$event
+    two(num, e){
+      e.preventDefault()
+    }
   }
+
 }
 </script>
 
