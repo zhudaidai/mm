faf3113044d72de4fcc18d8122e431de5d0eef5d
zhudaidai <1010462584@qq.com>
2022-06-29 14:45:35 +0800

    login新建第一次

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,23 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+   <h1>{{ msg }}</h1>
+    <h2>{{ obj.name }}</h2>
+    <h3>{{ obj.age > 18 ? '成年' : '未成年' }}</h3>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  data() { // 格式固定, 定义vue数据之处
+    return {  // key相当于变量名
+      msg: "hello, vue",
+      obj: {
+        name: "小vue",
+        age: 5
+      }
+    }
   }
 }
 </script>
