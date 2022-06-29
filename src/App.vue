2873b30d720a8018445ad8d07ab41e8a21b2a0bd
zhudaidai <1010462584@qq.com>
2022-06-29 16:40:09 +0800

    vue 绑定按键修饰符

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,25 @@
 <template>
   <div id="app">
-   <h1>{{ msg }}</h1>
-    <h2>{{ obj.name }}</h2>
-    <h3>{{ obj.age > 18 ? '成年' : '未成年' }}</h3>
-
-    <a v-bind:href="url">点击去百度</a>
-    <!-- 语法: :原生属性名="vue变量" -->
-    <img :src="imgUrl" />
-    <img :src="localImg">
+   <!-- 1. 绑定键盘按下事件.enter - 回车 
+         2. .esc修饰符 - 取消键-->
+    <input type="text" @keydown.enter="enterFn">
+    <hr>
+    <input type="text" @keydown.esc="escFn">
   </div>
 </template>
 
 <script>
-import imgObj from './assets/logo.png'
+
 
 export default {
-  data() { // 格式固定, 定义vue数据之处
-    return {  // key相当于变量名
-      msg: "hello, vue",
-      obj: {
-        name: "小vue",
-        age: 5,
-          },
-        url: 'http://www.baidu.com',
-        imgUrl: "http://yun.itheima.com/Upload/./Images/20210412/60741c11ab77b.jpg",
-        localImg: imgObj
-    }
-      
-  }
+  methods: {
+   enterFn(){
+     console.log("enter回车按键了");
+   },
+   escFn(){
+     console.log("esc按键了");
+   }
+ }
 }
 </script>
 
